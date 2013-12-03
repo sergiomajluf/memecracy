619916f10c88afe374e7b1112f931bb5fa7771a3
Sergio Majluf <sergio.majluf@gmail.com>
2013-12-03 17:37:28 -0500

    Index

--- index.php
+++ index.php
@@ -4,6 +4,6 @@
 </head>
 
 <body>
-<iframe src="http://memecracy.co.s60114.gridserver.com/" style="border: 0; width: 100%; height: 100%">Your browser doesn't support iFrames.</iframe>
+<iframe src="http://memecracy.co.s60114.gridserver.com/" style="position:fixed; top:0px; left:0px; bottom:0px; right:0px; width:100%; height:100%; border:none; margin:0; padding:0; overflow:hidden; z-index:999999;">Your browser doesn't support iFrames.</iframe>
 </body>
 </html>
